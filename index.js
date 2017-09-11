0a6d90550b555ed8c04fb3c8dd0b179184bd6aa9
Chris Cardle <cdcardle@gmail.com>
2017-09-11 21:02:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function whisper(STRING) {
-  return STRING.toLowerCase()
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout("string") {
+  console.log("string")
 }
